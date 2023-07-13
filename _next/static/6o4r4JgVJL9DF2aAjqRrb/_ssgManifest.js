8b79dba0d1fd1aee75cb1632b6d3c864663f663c
jypark <jypark@t3q.com>
2023-07-13 14:02:20 +0900

    en update

--- _next/static/6o4r4JgVJL9DF2aAjqRrb/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]\u002Fdr-answer","\u002F[locale]\u002Fempty","\u002F[locale]","\u002F[locale]\u002Ft3q-ai","\u002F[locale]\u002Ft3q-search","\u002F[locale]\u002F404","\u002F[locale]\u002Fai-engine","\u002F[locale]\u002Fai-hunmin","\u002F[locale]\u002Fabout"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
